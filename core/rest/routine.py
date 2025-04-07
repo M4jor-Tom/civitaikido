9ffef781c072cb63b8ae3987c2eef903e3a6f2d0
tom <tom-vautray@hotmail.fr>
2025-04-07 21:55:52 +0200

    feat(core/rest/routine.py): return failed state with exception on TimeoutError

--- core/rest/routine.py
+++ core/rest/routine.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Form, File, UploadFile
 
 from core.constant import main
-from core.provider import get_routine_executor
-from core.service import RoutineExecutor
+from core.provider import get_routine_executor, get_state_manager
+from core.service import RoutineExecutor, StateManager
 
 routine_router = APIRouter()
 
@@ -12,6 +12,10 @@ async def inject_generate_extract(
         file: UploadFile = File(...),
         inject_seed: bool = False,
         close_browser_when_finished: bool = True,
-        routine_executor: RoutineExecutor = Depends(get_routine_executor)
+        routine_executor: RoutineExecutor = Depends(get_routine_executor),
+        state_manager: StateManager = Depends(get_state_manager)
     ):
-    await routine_executor.execute_routine(session_url, file, inject_seed, close_browser_when_finished)
+    try:
+        await routine_executor.execute_routine(session_url, file, inject_seed, close_browser_when_finished)
+    except TimeoutError as e:
+        return f"Failed on state {state_manager.injection_extraction_state.value} with exception: {e}"
